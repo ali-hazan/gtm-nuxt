c8a8e1cccb46e01129602ff1219f0431c0cbc5b1
Ali <ali.hassan@rhinos-me.com>
2023-05-26 17:59:26 +0400

    Initial commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="triggerEvent">Track me!</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useGtm } from '@gtm-support/vue-gtm';
+
+const gtm = useGtm();
+
+function triggerEvent() {
+  gtm.trackEvent({
+    event: 'event name',
+    category: 'category',
+    action: 'click',
+    label: 'My custom component trigger',
+    value: 5000,
+    noninteraction: false,
+  });
+}
+
+
+
+
+</script>
